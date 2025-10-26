954831386dee46cdaae0e0f2ef0be52c5ae77453
Hassan Yakubu <yhassan677@gmail.com>
2025-10-26 20:53:41 +0000

    error logging

--- actions/upload_product_image_action.php
+++ actions/upload_product_image_action.php
@@ -1,7 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', '/home/hassan.yakubu/public_html/logs/php_error.log');
 error_reporting(E_ALL);
 
 // Ensure clean JSON only
